10c692f1bb287339c1f59b238e7607c29481c859
gnoxy-dev <dev@gnoxy.com>
2017-11-26 21:01:28 -0600

    Added files

---
+++ src/main/java/com/gnoxy/restclient/App.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gnoxy.restclient;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ *
+ * @author gnoxy
+ */
+public class App {
+
+    public static void main(String args[]) {
+        String ServiceURL = "http://u_dev:8080/RestServer/rest/services/";
+
+        Client client = ClientBuilder.newClient();
+
+        String helloWorldString = client.target(ServiceURL + "helloWorld") // connection to the pre-defined URL
+                .request("text/plain") // we request a "text/plain" answer
+                .get(String.class);
+
+        System.out.println("helloWorld response: " + helloWorldString);
+
+        List<String> helloJSONString = client.target(ServiceURL + "helloJSON") // connection to the pre-defined URL
+                .request()
+                .get(ArrayList.class);
+        System.out.println("helloJSON response:");
+        for (int i=0; i < helloJSONString.size(); i++) {
+            System.out.println("   " + helloJSONString.get(i));
+        }
+    }
+
+}
